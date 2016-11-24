b945cb3fff67f1f212ab06fdaa428b22cee456fc
Ricardo de Azambuja <ricardo.azambuja@gmail.com>
2016-11-24 11:06:36 +0000

    fixes and now it can return the raw frame

--- Webcam2HTTP.py
+++ Webcam2HTTP.py
@@ -25,44 +25,50 @@ def snapshot_generator(device=0, sleep=0.25, img_format = 'PNG', img_quality=0, 
     or with False to close the device. Closing the device reduces the max frequency.
     '''
     
-    
-    capture = cv2.VideoCapture(device)
-    
-    time.sleep(sleep)
-    
-    if capture.isOpened():
-        print "Device initialised, ready to capture!"
-    else:
-        print "Device failed..."
-        return -1
+    if not debug:
+        capture = cv2.VideoCapture(device)
+
+        time.sleep(sleep)
+
+        if capture.isOpened():
+            print "Device initialised, ready to capture!"
+        else:
+            print "Device failed..."
+            return -1
     
     def capture_image_gen(debug,filename):
         while True:
             if debug and filename!=None:
-                frame = cv2.imread(os.getcwd()+'/'+filename)
+                frame = cv2.imread(filename)
             else:
                 grabbed, frame = capture.read()
 
             if img_format == 'PNG':
                 if 1>=img_quality>=0:
                     yield cv2.imencode('.png', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 
-                                       [cv2.cv.CV_IMWRITE_PNG_COMPRESSION, img_quality])[1].tobytes()
+                                       [cv2.cv.CV_IMWRITE_PNG_COMPRESSION, img_quality])[1].tobytes(),frame
             elif img_format == 'JPG':
                 if 100>=img_quality>=0:
                     yield cv2.imencode('.png', cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), 
-                                       [cv2.cv.CV_IMWRITE_JPEG_QUALITY, img_quality])[1].tobytes()
+                                       [cv2.cv.CV_IMWRITE_JPEG_QUALITY, img_quality])[1].tobytes(),frame
+                    
             else:
                 raise Exception("Error! Image format (img_format) must be \'PNG\' or \'JPG\'!")
 
                 
     init_gen = capture_image_gen(debug,filename)  
     
-    def wrapper(cmd=True):
+    def wrapper(cmd=True,ret_orig=False):
         if cmd:
-            return init_gen.next()
+            if ret_orig:
+                return init_gen.next()
+            else:
+                return init_gen.next()[0]
         else:
-            capture.release() # closes the camera, video file, etc
-            print "Device closed, exiting..."
+            if not debug:
+                capture.release() # closes the camera, video file, etc
+                print "Device closed, exiting..."
             return 0
+
         
     return wrapper
